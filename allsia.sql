b32ecd68363f8f02797ecbf894d6d302cb04c3ec
anotherinthefire <rongodfreyultra@gmail.com>
2023-04-07 18:33:29 +0800

    newdb

--- allsia.sql
+++ allsia.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 29, 2023 at 06:19 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Generation Time: Apr 06, 2023 at 12:57 PM
+-- Server version: 10.4.16-MariaDB
+-- PHP Version: 7.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `allsia`
+-- Database: `sia`
 --
 
 -- --------------------------------------------------------
@@ -58,32 +58,17 @@ CREATE TABLE `admin` (
   `user_level` varchar(50) NOT NULL,
   `name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `contact` varchar(50) NOT NULL,
   `cr_date` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `carousel_images`
+-- Dumping data for table `admin`
 --
 
-CREATE TABLE `carousel_images` (
-  `id` int(11) NOT NULL,
-  `filename` varchar(255) NOT NULL,
-  `caption` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `carousel_images`
---
-
-INSERT INTO `carousel_images` (`id`, `filename`, `caption`) VALUES
-(1, 'ad.png', 'hanni'),
-(2, 'ads.png', 'minarie'),
-(3, 'ads.png', 'abie'),
-(4, 'adsss.png', '4th pic');
+INSERT INTO `admin` (`admin_id`, `user_level`, `name`, `email`, `password`, `contact`, `cr_date`) VALUES
+(1, 'new_trend', 'Andre Paul N. Sta. Clara', 'andrepaul.staclara67@gmail.com', '$2y$10$k3HnMwBvYei6PyivFU92UeZSpDFeI2rT/fiRR/GsiLpeswxJu7Po6', '09298410728', '2023-03-24 10:53:55');
 
 -- --------------------------------------------------------
 
@@ -94,19 +79,10 @@ INSERT INTO `carousel_images` (`id`, `filename`, `caption`) VALUES
 CREATE TABLE `cart` (
   `cart_id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
-  `prod_id` int(11) DEFAULT NULL,
-  `col_id` int(11) DEFAULT NULL,
-  `size_id` int(11) DEFAULT NULL,
+  `stock_id` int(11) DEFAULT NULL,
   `quantity` int(11) NOT NULL,
   `cart_date` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `cart`
---
-
-INSERT INTO `cart` (`cart_id`, `user_id`, `prod_id`, `col_id`, `size_id`, `quantity`, `cart_date`) VALUES
-(1, 1, 1, 1, 2, 5, '2023-03-22 16:10:29');
 
 -- --------------------------------------------------------
 
@@ -115,17 +91,9 @@ INSERT INTO `cart` (`cart_id`, `user_id`, `prod_id`, `col_id`, `size_id`, `quant
 --
 
 CREATE TABLE `category` (
-  `cat_id` int(11) NOT NULL,
-  `cat_name` varchar(50) NOT NULL
+  `category_id` int(11) NOT NULL,
+  `category` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `category`
---
-
-INSERT INTO `category` (`cat_id`, `cat_name`) VALUES
-(1, 'Shirt'),
-(2, 'Lanyard');
 
 -- --------------------------------------------------------
 
@@ -134,17 +102,9 @@ INSERT INTO `category` (`cat_id`, `cat_name`) VALUES
 --
 
 CREATE TABLE `color` (
-  `col_id` int(11) NOT NULL,
-  `prod_id` int(11) DEFAULT NULL,
+  `color_id` int(11) NOT NULL,
   `color` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `color`
---
-
-INSERT INTO `color` (`col_id`, `prod_id`, `color`) VALUES
-(1, 1, 'Red');
 
 -- --------------------------------------------------------
 
@@ -176,18 +136,9 @@ INSERT INTO `messages` (`mess_id`, `mess_name`, `email`, `message`, `date_sent`)
 CREATE TABLE `ordered_products` (
   `oprod_id` int(11) NOT NULL,
   `ord_id` int(11) DEFAULT NULL,
-  `prod_id` int(11) DEFAULT NULL,
-  `col_id` int(11) DEFAULT NULL,
-  `size_id` int(11) DEFAULT NULL,
+  `stock_id` int(11) DEFAULT NULL,
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `ordered_products`
---
-
-INSERT INTO `ordered_products` (`oprod_id`, `ord_id`, `prod_id`, `col_id`, `size_id`, `quantity`) VALUES
-(1, 1, 1, 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -224,21 +175,11 @@ INSERT INTO `orders` (`ord_id`, `user_id`, `add_id`, `full_name`, `contact`, `mo
 
 CREATE TABLE `products` (
   `prod_id` int(11) NOT NULL,
-  `cat_id` int(11) DEFAULT NULL,
   `prod_name` varchar(50) NOT NULL,
-  `prod_price` decimal(7,2) NOT NULL,
+  `prod_price` double(7,2) NOT NULL,
   `prod_desc` text NOT NULL,
-  `prod_img` varchar(255) NOT NULL
+  `prod_img` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `products`
---
-
-INSERT INTO `products` (`prod_id`, `cat_id`, `prod_name`, `prod_price`, `prod_desc`, `prod_img`) VALUES
-(1, 1, 'AXGG \' One Piece - Zoro Asura \' Long Sleeve', '1262.00', '✔️ A perfect merch for gamers and anime lovers to express their passion\r\n✔️ AXGG collection is inspired by the playful spirit found in anime and video games while still fitting into any lifestyle.\r\n✔️ Activewear is one of the most functional garments, but it is also important to have a means of expressing your personality.\r\n✔️ It\'s 100% high-quality premium polyester fabric, so you can wear it with confidence knowing that it will keep you dry and comfortable in any situation.\r\n✔️ This T-shirt also comes with an absorption technology designed specifically to wick sweat away from your body and keep you comfortable throughout your next adventure!\r\n✔️ Using Ultra-High Quality EPSON Printing System ensures that the color of shirts will never fade away\r\n✔️ Our range of size are available from kids size to 3XL. We have the larger sizes for collectors who like to display their favorite characters proudly!\r\n✔️ You deserve the best and we promise to do everything we can to bring you high quality Anime Merchandise. If you are not satisfied with your orders or have any issues, please contact us and we will work with you to ensure that you are satisfied!', 'axgglogo.jpg'),
-(2, 2, 'QCU', '99.00', 'QCU Lanyard by css', 'axgglogo.jpg'),
-(3, 1, 'EXTN', '599.00', 'maganda', 'axgglogo.jpg');
 
 -- --------------------------------------------------------
 
@@ -249,38 +190,46 @@ INSERT INTO `products` (`prod_id`, `cat_id`, `prod_name`, `prod_price`, `prod_de
 CREATE TABLE `proof` (
   `proof_id` int(11) NOT NULL,
   `ord_id` int(11) DEFAULT NULL,
-  `pay_img` varchar(255) DEFAULT NULL,
-  `pay_date` datetime DEFAULT NULL,
-  `pick_img` varchar(255) DEFAULT NULL,
-  `pick_date` datetime DEFAULT NULL,
-  `deli_img` varchar(255) DEFAULT NULL,
-  `deli_date` datetime DEFAULT NULL
+  `payment_img` varchar(255) DEFAULT NULL,
+  `payment_date` datetime DEFAULT NULL,
+  `pickup_img` varchar(255) DEFAULT NULL,
+  `pickup_date` datetime DEFAULT NULL,
+  `delivered_img` varchar(255) DEFAULT NULL,
+  `delivered_date` datetime DEFAULT NULL,
+  `remarks` text DEFAULT NULL,
+  `remarks_img` varchar(255) DEFAULT NULL,
+  `remarks_date` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sizes`
+-- Table structure for table `size`
 --
 
-CREATE TABLE `sizes` (
+CREATE TABLE `size` (
   `size_id` int(11) NOT NULL,
-  `col_id` int(11) DEFAULT NULL,
-  `size_code` varchar(5) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `barcode` varchar(255) NOT NULL,
-  `status` tinyint(4) DEFAULT 0,
-  `sales` int(11) NOT NULL DEFAULT 0,
-  `date_added` datetime NOT NULL DEFAULT current_timestamp()
+  `size` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `sizes`
+-- Table structure for table `stock`
 --
 
-INSERT INTO `sizes` (`size_id`, `col_id`, `size_code`, `quantity`, `barcode`, `status`, `sales`, `date_added`) VALUES
-(1, 1, 'S', 25, 'test', 0, 0, '2023-03-22 16:03:30'),
-(2, 1, 'M', 24, 'test', 0, 1, '2023-03-22 16:03:30');
+CREATE TABLE `stock` (
+  `stock_id` int(11) NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `prod_id` int(11) DEFAULT NULL,
+  `color_id` int(11) DEFAULT NULL,
+  `size_id` int(11) DEFAULT NULL,
+  `stock` int(11) NOT NULL,
+  `sales` int(11) NOT NULL DEFAULT 0,
+  `barcode` varchar(255) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 0,
+  `date_added` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -303,8 +252,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `fullname`, `username`, `password`, `email`, `contact`, `cr_date`) VALUES
-(1, 'Bogart', 'bogart123', 'bogart123', 'bogart123@gmail.com', '09123456789', '2023-03-22 15:53:37'),
-(2, 'Ron Ultra', 'suntzaur', '$2y$10$yrvpzhX2lQ7BqBEddspcPO.8VPmR1UkfiRFms/M.8PZ', 'rongodfreyultra@gmail.com', '+639270360320', '2023-03-24 13:55:52');
+(1, 'Bogart Pedring', 'bogart123', 'bogart123', 'bogart123@gmail.com', '09123456789', '2023-03-22 15:53:37');
 
 --
 -- Indexes for dumped tables
@@ -324,33 +272,24 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`admin_id`);
 
 --
--- Indexes for table `carousel_images`
---
-ALTER TABLE `carousel_images`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `cart`
 --
 ALTER TABLE `cart`
   ADD PRIMARY KEY (`cart_id`),
   ADD KEY `user_id` (`user_id`),
-  ADD KEY `prod_id` (`prod_id`),
-  ADD KEY `col_id` (`col_id`),
-  ADD KEY `size_id` (`size_id`);
+  ADD KEY `stock_id` (`stock_id`);
 
 --
 -- Indexes for table `category`
 --
 ALTER TABLE `category`
-  ADD PRIMARY KEY (`cat_id`);
+  ADD PRIMARY KEY (`category_id`);
 
 --
 -- Indexes for table `color`
 --
 ALTER TABLE `color`
-  ADD PRIMARY KEY (`col_id`),
-  ADD KEY `prod_id` (`prod_id`);
+  ADD PRIMARY KEY (`color_id`);
 
 --
 -- Indexes for table `messages`
@@ -364,9 +303,7 @@ ALTER TABLE `messages`
 ALTER TABLE `ordered_products`
   ADD PRIMARY KEY (`oprod_id`),
   ADD KEY `ord_id` (`ord_id`),
-  ADD KEY `prod_id` (`prod_id`),
-  ADD KEY `col_id` (`col_id`),
-  ADD KEY `size_id` (`size_id`);
+  ADD KEY `stock_id` (`stock_id`);
 
 --
 -- Indexes for table `orders`
@@ -380,8 +317,7 @@ ALTER TABLE `orders`
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`prod_id`),
-  ADD KEY `cat_id` (`cat_id`);
+  ADD PRIMARY KEY (`prod_id`);
 
 --
 -- Indexes for table `proof`
@@ -391,11 +327,20 @@ ALTER TABLE `proof`
   ADD KEY `ord_id` (`ord_id`);
 
 --
--- Indexes for table `sizes`
+-- Indexes for table `size`
 --
-ALTER TABLE `sizes`
-  ADD PRIMARY KEY (`size_id`),
-  ADD KEY `col_id` (`col_id`);
+ALTER TABLE `size`
+  ADD PRIMARY KEY (`size_id`);
+
+--
+-- Indexes for table `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`stock_id`),
+  ADD KEY `category_id` (`category_id`),
+  ADD KEY `prod_id` (`prod_id`),
+  ADD KEY `color_id` (`color_id`),
+  ADD KEY `size_id` (`size_id`);
 
 --
 -- Indexes for table `user`
@@ -417,31 +362,25 @@ ALTER TABLE `address`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `carousel_images`
---
-ALTER TABLE `carousel_images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `color`
 --
 ALTER TABLE `color`
-  MODIFY `col_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `color_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `messages`
@@ -453,7 +392,7 @@ ALTER TABLE `messages`
 -- AUTO_INCREMENT for table `ordered_products`
 --
 ALTER TABLE `ordered_products`
-  MODIFY `oprod_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `oprod_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `orders`
@@ -465,7 +404,7 @@ ALTER TABLE `orders`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `prod_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `prod_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `proof`
@@ -474,16 +413,22 @@ ALTER TABLE `proof`
   MODIFY `proof_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `sizes`
+-- AUTO_INCREMENT for table `size`
 --
-ALTER TABLE `sizes`
-  MODIFY `size_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `size`
+  MODIFY `size_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `stock`
+--
+ALTER TABLE `stock`
+  MODIFY `stock_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -500,24 +445,14 @@ ALTER TABLE `address`
 --
 ALTER TABLE `cart`
   ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`prod_id`) REFERENCES `products` (`prod_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `cart_ibfk_3` FOREIGN KEY (`col_id`) REFERENCES `color` (`col_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `cart_ibfk_4` FOREIGN KEY (`size_id`) REFERENCES `sizes` (`size_id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
---
--- Constraints for table `color`
---
-ALTER TABLE `color`
-  ADD CONSTRAINT `color_ibfk_1` FOREIGN KEY (`prod_id`) REFERENCES `products` (`prod_id`) ON DELETE SET NULL ON UPDATE CASCADE;
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`stock_id`) REFERENCES `stock` (`stock_id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `ordered_products`
 --
 ALTER TABLE `ordered_products`
   ADD CONSTRAINT `ordered_products_ibfk_1` FOREIGN KEY (`ord_id`) REFERENCES `orders` (`ord_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `ordered_products_ibfk_2` FOREIGN KEY (`prod_id`) REFERENCES `products` (`prod_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `ordered_products_ibfk_3` FOREIGN KEY (`col_id`) REFERENCES `color` (`col_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `ordered_products_ibfk_4` FOREIGN KEY (`size_id`) REFERENCES `sizes` (`size_id`) ON DELETE SET NULL ON UPDATE CASCADE;
+  ADD CONSTRAINT `ordered_products_ibfk_2` FOREIGN KEY (`stock_id`) REFERENCES `stock` (`stock_id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `orders`
@@ -527,22 +462,19 @@ ALTER TABLE `orders`
   ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`add_id`) REFERENCES `address` (`add_id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
--- Constraints for table `products`
---
-ALTER TABLE `products`
-  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`cat_id`) REFERENCES `category` (`cat_id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
---
 -- Constraints for table `proof`
 --
 ALTER TABLE `proof`
   ADD CONSTRAINT `proof_ibfk_1` FOREIGN KEY (`ord_id`) REFERENCES `orders` (`ord_id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
--- Constraints for table `sizes`
+-- Constraints for table `stock`
 --
-ALTER TABLE `sizes`
-  ADD CONSTRAINT `sizes_ibfk_1` FOREIGN KEY (`col_id`) REFERENCES `color` (`col_id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `stock`
+  ADD CONSTRAINT `stock_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `stock_ibfk_2` FOREIGN KEY (`prod_id`) REFERENCES `products` (`prod_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `stock_ibfk_3` FOREIGN KEY (`color_id`) REFERENCES `color` (`color_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `stock_ibfk_4` FOREIGN KEY (`size_id`) REFERENCES `size` (`size_id`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
