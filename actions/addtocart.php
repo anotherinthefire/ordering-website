d9e1c1b91657404753cebbf52ba2c2dd5887d5ec
anotherinthefire <rongodfreyultra@gmail.com>
2023-07-26 14:39:11 +0800

    readin

--- actions/addtocart.php
+++ actions/addtocart.php
@@ -1,13 +1,11 @@
 <?php
 try {
   include('../config.php');
-}
-catch (Exception $e) {
+} catch (Exception $e) {
   echo "ERROR: Could not include config file. " . $e->getMessage();
 }
 
-if(isset($_POST['submit'])) 
-{
+if (isset($_POST['submit'])) {
   $user_id = $_SESSION['user_id'];
   $stock_id = $_POST['stock_id'];
   $quantity = $_POST['quantity'];
@@ -15,28 +13,25 @@ if(isset($_POST['submit']))
   // Check if the user_id and stock_id already exist in the cart
   $sql = "SELECT * FROM cart WHERE user_id = $user_id AND stock_id = $stock_id";
   $result = $conn->query($sql);
-  if($result->num_rows > 0) {
+  if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
     $new_quantity = $row['quantity'] + $quantity;
-    if($new_quantity <= 0) {
+    if ($new_quantity <= 0) {
       echo '<script>alert("Quantity must be greater than 0."); window.history.back();</script>';
       exit;
     }
     $sql = "UPDATE cart SET quantity = $new_quantity WHERE user_id = $user_id AND stock_id = $stock_id";
   } else {
-    if($quantity <= 0) {
+    if ($quantity <= 0) {
       echo '<script>alert("Quantity must be greater than 0."); window.history.back();</script>';
       exit;
     }
     $sql = "INSERT INTO cart (user_id, stock_id, quantity) VALUES ('$user_id', '$stock_id', '$quantity')";
   }
 
-  if($conn->query($sql) === true)
-  {
-    header("location:../pages/cart.php");                            
-  }
-  else
-  {
+  if ($conn->query($sql) === true) {
+    header("location:../pages/cart.php");
+  } else {
     echo "ERROR: Could not able to execute $sql. " . $conn->error;
   }
 }
--- pages/cart.php
+++ pages/cart.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AXGG | Cart</title>
-    <link rel="shortcut icon" href="https://i.ibb.co/dfD3s4M/278104398-126694786613134-4231769107383237629-n-removebg-preview.png" />
+    <link rel="shortcut icon"
+        href="https://i.ibb.co/dfD3s4M/278104398-126694786613134-4231769107383237629-n-removebg-preview.png" />
     <link rel="stylesheet" href="../assets/css/cart.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous" />
 </head>
 
 <body>
-    <?php include("../includes/nav-pages.php"); ?>
+    <?php include "../includes/nav-pages.php"; ?>
     <br><br><br><br>
 
     <div style="text-align:center;">
@@ -20,67 +23,85 @@
         <br>
         <?php
         $user_id = $_SESSION['user_id'];
-            $sql = "SELECT count(*) as Total FROM cart WHERE user_id =$user_id";//query
-            $result = mysqli_query($conn, $sql);
-            if (mysqli_num_rows($result) > 0) {
-                $row = mysqli_fetch_assoc($result);
-                $total = $row['Total'];
-                    ?>
-                    <table class="pure-table" style="margin-left: 100px;margin-right: 100px;">
-                        <tbody style="border-left-width: 0px;">
-                            <tr class="item" style="font-size:20px;">
-                                <td>Items (Total - <?php echo $total; ?>)</td>
-                                <td></td>
-                                <td></td>
-                                <td>Quantity</td>
-                                <td style="padding-left: 60px;">Total</td>
-                                <td></td>
-                            </tr>
-                            <form id="checkprod" action ="../actions/tocheckout.php" method="post">
-                <?php
-                }                         
-                    $query = "SELECT cart.cart_id, size.size_id, color.color_id, size.size, color.color, quantity, products.prod_img, products.prod_price as productPrice, products.prod_name, (quantity * prod_price) as totalPrice 
-                                        FROM cart 
-                                        JOIN stock ON stock.stock_id = cart.stock_id 
-                                        JOIN products ON products.prod_id = stock.prod_id  
+        $sql = "SELECT count(*) as Total FROM cart WHERE user_id =$user_id"; //query
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            $total = $row['Total'];
+            ?>
+            <table class="pure-table" style="margin-left: 100px;margin-right: 100px;">
+                <tbody style="border-left-width: 0px;">
+                    <tr class="item" style="font-size:20px;">
+                        <td>Items (Total -
+                            <?php echo $total; ?>)
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td>Quantity</td>
+                        <td style="padding-left: 60px;">Total</td>
+                        <td></td>
+                    </tr>
+                    <form id="checkprod" action="../actions/tocheckout.php" method="post">
+                        <?php
+        }
+        $query = "SELECT cart.cart_id, size.size_id, color.color_id, size.size, color.color, quantity, products.prod_img, products.prod_price as productPrice, products.prod_name, (quantity * prod_price) as totalPrice
+                                        FROM cart
+                                        JOIN stock ON stock.stock_id = cart.stock_id
+                                        JOIN products ON products.prod_id = stock.prod_id
                                         JOIN size ON size.size_id = stock.size_id
                                         JOIN color ON color.color_id = stock.color_id
-                                        WHERE user_id = $user_id";         
-                    $result = mysqli_query($conn, $query);
-                    if (mysqli_num_rows($result) > 0) {//cond start
-                        while ($row = mysqli_fetch_array($result)) {//loop start
-                        ?>
-                        <tr class="Katekyo" style="border-bottom-width: 4.667; padding-bottom:10px;">
-                        <td style="padding-left: 75px;"> <input type="checkbox" name="cart_item[]" value="<?php echo $row['cart_id']; ?>"></td>
-                        <td style="padding-bottom: 30px;padding-top: 30px;">
-                        <img src="../../product_images/<?php echo $row['prod_img']; ?>" style="width: 200px; height: 200px;">
-                        </td>
-                    <td class="Hit" style="padding-left: 10px;">
-                        <b><?php echo $row['prod_name']; ?></b>
-                        <p>Size(<?php echo $row['size']; ?>)</p>
-                        <p>Color(<?php echo $row['color']; ?>)</p>
-                        <p>Price ₱<?php echo $row['productPrice']; ?></p>
-                    </td>
-                <!-- next -->
-                    <td>
-                
-                        <input type="hidden" name="cart_id" value="<?php echo $row['cart_id']; ?>">
-                        <button type="submit" name="minus" style="background-color:transparent; border:none;">-</button>
-                        <input type="text" id="quantity" name="quantity" value="<?php echo $row['quantity']; ?>" style="text-align: center; width:7vw;border-color: transparent;" readonly>
-                        <button type="submit" name="plus" style="background-color:transparent; border:none;">+</button>
-               
-                    </td>
-                    <td style="padding-left: 20px; padding-right: 20px; padding-bottom: 0px;">
-                    <div id="">₱<?php echo $row['totalPrice']; ?></div>
-                    </td><td>
-                
-                    <button type="submit" name="delete" class="btn btn-white">X</button>
-                    <input type='text' hidden name='cart_id' value="<?php echo $row['cart_id']; ?>">
-                
-                    </td>
-                </tr>
-        <?php
-            }//loop end
+                                        WHERE user_id = $user_id";
+        $result = mysqli_query($conn, $query);
+        if (mysqli_num_rows($result) > 0) { //cond start
+            while ($row = mysqli_fetch_array($result)) { //loop start
+                ?>
+                            <tr class="Katekyo" style="border-bottom-width: 4.667; padding-bottom:10px;">
+                                <td style="padding-left: 75px;"> <input type="checkbox" name="cart_item[]"
+                                        value="<?php echo $row['cart_id']; ?>"></td>
+                                <td style="padding-bottom: 30px;padding-top: 30px;">
+                                    <img src="../../product_images/<?php echo $row['prod_img']; ?>"
+                                        style="width: 200px; height: 200px;">
+                                </td>
+                                <td class="Hit" style="padding-left: 10px;">
+                                    <b>
+                                        <?php echo $row['prod_name']; ?>
+                                    </b>
+                                    <p>Size(
+                                        <?php echo $row['size']; ?>)
+                                    </p>
+                                    <p>Color(
+                                        <?php echo $row['color']; ?>)
+                                    </p>
+                                    <p>Price ₱
+                                        <?php echo $row['productPrice']; ?>
+                                    </p>
+                                </td>
+                                <!-- next -->
+                                <td>
+
+                                    <input type="hidden" name="cart_id" value="<?php echo $row['cart_id']; ?>">
+                                    <button type="submit" name="minus"
+                                        style="background-color:transparent; border:none;">-</button>
+                                    <input type="text" id="quantity" name="quantity" value="<?php echo $row['quantity']; ?>"
+                                        style="text-align: center; width:7vw;border-color: transparent;" readonly>
+                                    <button type="submit" name="plus"
+                                        style="background-color:transparent; border:none;">+</button>
+
+                                </td>
+                                <td style="padding-left: 20px; padding-right: 20px; padding-bottom: 0px;">
+                                    <div id="">₱
+                                        <?php echo $row['totalPrice']; ?>
+                                    </div>
+                                </td>
+                                <td>
+
+                                    <button type="submit" name="delete" class="btn btn-white">X</button>
+                                    <input type='text' hidden name='cart_id' value="<?php echo $row['cart_id']; ?>">
+
+                                </td>
+                            </tr>
+                            <?php
+            } //loop end
             if (isset($_POST['checkout'])) {
                 // Get user status from database
                 $email = $_SESSION['email'];
@@ -88,7 +109,7 @@
                 $result = mysqli_query($conn, $sql);
                 $row = mysqli_fetch_assoc($result);
                 $user_status = $row['status'];
-            
+
                 if ($user_status == 0) {
                     echo "<script>alert('Error: You cannot checkout because your account is not verified.')</script>";
                 } else {
@@ -100,63 +121,61 @@
                         echo "<script>alert('Error: No items were selected for checkout.')</script>";
                     }
                 }
-            }}
-            
-        
-            
-            ?>
-            
-                        </tbody>
-                    </table>
-                    <br>
-                    <?php
-                    if (isset($_SESSION['user_id'])) {
-                        $query = "SELECT cart.cart_id, size.size_id, color.color_id, 
-                        size.size, color.color, cart.quantity, products.prod_img, 
-                        products.prod_price as productPrice, products.prod_name, 
-                        (cart.quantity * products.prod_price) as totalPrice 
-                        FROM cart 
-                        JOIN stock ON stock.stock_id = cart.stock_id 
-                        JOIN products ON products.prod_id = stock.prod_id 
+            }
+        }
+
+        ?>
+
+            </tbody>
+        </table>
+        <br>
+        <?php
+        if (isset($_SESSION['user_id'])) {
+            $query = "SELECT cart.cart_id, size.size_id, color.color_id,
+                        size.size, color.color, cart.quantity, products.prod_img,
+                        products.prod_price as productPrice, products.prod_name,
+                        (cart.quantity * products.prod_price) as totalPrice
+                        FROM cart
+                        JOIN stock ON stock.stock_id = cart.stock_id
+                        JOIN products ON products.prod_id = stock.prod_id
                         JOIN size ON size.size_id = stock.size_id
                         JOIN color ON color.color_id = stock.color_id
                         WHERE cart.user_id = " . $_SESSION["user_id"];
-                        $result = mysqli_query($conn, $query);
-                        if (mysqli_num_rows($result) > 0) {
-                            $finalPrice = 0;
-                    ?>
-                            <!-- <?php while ($row = mysqli_fetch_array($result)) { ?>
+            $result = mysqli_query($conn, $query);
+            if (mysqli_num_rows($result) > 0) {
+                $finalPrice = 0;
+                ?>
+                <!-- <?php while ($row = mysqli_fetch_array($result)) { ?>
                                 <p><?php echo $row['prod_name'] . ' - ' . $row['quantity'] . ' x P' . $row['productPrice']; ?></p>
                                 <?php $finalPrice += $row['totalPrice']; ?>
                             <?php } ?> -->
 
-                            <p class="subtotal"><b>Subtotal: P<?php echo $finalPrice; ?></b></p>
-                            <p class="Excluding"><br>Excluding of taxes and shipping</p>
-                            <br><br><br>
+                <p class="subtotal"><b>Subtotal: P
+                        <?php echo $finalPrice; ?>
+                    </b></p>
+                <p class="Excluding"><br>Excluding of taxes and shipping</p>
+                <br><br><br>
 
-                            <a href="collection.php">
-                                <button type="button" class="btn btn-light" style="border:2px solid #0dcaf0;">
-                                    Continue Shoping
-                                </button>
-                            </a>
-                            <button type="submit" name="checkout" value="Checkout" form="checkprod" class="btn btn-Primary" style="
-             padding-left: 25px;
-             padding-right: 25px;
-             border:2px solid transparent;
-             ">Checkout</button>    
-             
-                            <br><br><br>
+                <a href="collection.php">
+                    <button type="button" class="btn btn-light" style="border:2px solid #0dcaf0;">
+                        Continue Shoping
+                    </button>
+                </a>
+                <button type="submit" name="checkout" value="Checkout" form="checkprod" class="btn btn-Primary"
+                    style="padding-left: 25px; padding-right: 25px;border:2px solid transparent;">Checkout</button>
+
+                <br><br><br>
                 </form>
-                    <?php
-                        }
-                    }
-                    ?>
-        </div>
+                <?php
+            }
+        }
+        ?>
+    </div>
     <br>
     <!-- <script src="../assets/js/cart.js"></script> -->
 
     <?php
-    include("../includes/foot-pages.php");
+    include "../includes/foot-pages.php";
     ?>
 
 </body>
